c9f2237aaa901163ea1efea01a1f76fd97e508a1
nuttawoot <nuttawoot.s@app-alive.com>
2017-05-13 09:59:32 +0700

    handle dev/prod connection

--- src/DbConnection.php
+++ src/DbConnection.php
@@ -2,17 +2,19 @@
 require 'vendor/autoload.php';
 
 final class DbConnection {
-
   public static function getConnection () {
-    $driver = 'mysqli';
-    $server = '54.169.33.80';
-    $user = 'root';
-    $password = '1qaz2wsx';
-    $database = 'agile-php';
+    $development = array("driver"=>"mysqli", "server"=>"localhost", "user"=>"root", "password"=>"Zaq12wsx", "database"=>"agile-php");
+    $production = array("driver"=>"mysqli", "server"=>"54.169.33.80", "user"=>"root", "password"=>"1qaz2wsx", "database"=>"agile-php");
+    
+    echo "server name is: ".$_SERVER['SERVER_NAME'];
+    $config = $production;
+    if ($_SERVER['SERVER_NAME'] == 'localhost') {
+      $config = $development;
+    }
 
-    $db = ADONewConnection($driver);
+    $db = ADONewConnection($config['driver']);
     $db->debug = false;
-    $db->Connect($server, $user, $password, $database);
+    $db->Connect($config['server'], $config['user'], $config['password'], $config['database']);
     return $db;
   }
 }
